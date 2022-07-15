f8bc3df00570739925f007392f7df3d25cf34701
Andy-git985 <andy.yu617@gmail.com>
2022-07-14 22:09:19 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const todoItem = (title, description, dueDate, priority) => {
+  const getTitle = () => title;
+  const getDescription = () => description;
+  const getDueDate = () => dueDate;
+  const getPriority = () => priority;
+  return {
+    getTitle,
+    getDescription,
+    getDueDate,
+    getPriority,
+  };
+};
+
+// const myLife = todoItem(
+//   'my life',
+//   'delete information off of my life website',
+//   new Date(2022, 7, 21),
+//   'Urgent'
+// );
+
+const toDoList = [];
+const createToDoItem = (...item) => {
+  const newItem = todoItem(...item);
+  // console.log(newItem.getTitle());
+  // console.log(newItem.getDescription());
+  // console.log(newItem.getDueDate());
+  // console.log(newItem.getPriority());
+  return newItem;
+};
+
+const addToDoItem = (item) => {
+  toDoList.push(item);
+};
+
+const newItem = createToDoItem(
+  'clean',
+  'clean the place',
+  new Date(2022, 7, 14),
+  'Not Urgent'
+);
+addToDoItem(newItem);
+addToDoItem(
+  createToDoItem(
+    'returns',
+    'return my online shopping',
+    new Date(2022, 7, 17),
+    'Urgent'
+  )
+);
+
+const printToDo = (arr) => {
+  arr.forEach((obj) => {
+    Object.values(obj).forEach((func) => console.log(func()));
+  });
+};
+
+printToDo(toDoList);
