00479470edfe5763bdeb6095712ea14b588496bd
Andy-git985 <andy.yu617@gmail.com>
2022-07-15 23:27:40 -0400

    project boiler plate

--- src/dom.js
+++ src/dom.js
@@ -2,6 +2,20 @@ const dom = (() => {
   const clear = (selector) => {
     document.querySelector(selector).textContent = '';
   };
+  const projBoilerPlate = (selector) => {
+    const label = document.createElement('label');
+    label.setAttribute('for', 'projects');
+    label.textContent = 'Project';
+    document.querySelector(selector).appendChild(label);
+    const select = document.createElement('select');
+    select.setAttribute('name', 'projects');
+    const option = document.createElement('option');
+    option.setAttribute('value', '');
+    option.textContent = 'Select one';
+    select.appendChild(option);
+    select.insertAdjacentElement('beforebegin', label);
+    return select;
+  };
   const render = (selector, arr) => {
     clear(selector);
     const todos = document.querySelector(selector);
@@ -10,6 +24,7 @@ const dom = (() => {
       if (typeof obj !== 'string') {
         for (const [key, value] of Object.entries(obj)) {
           const innerDiv = document.createElement('div');
+          console.log(typeof value);
           innerDiv.textContent = `${key}: ${value}`;
           outerDiv.appendChild(innerDiv);
         }
@@ -20,17 +35,13 @@ const dom = (() => {
     }
   };
   const renderFormProjects = (selector, arr) => {
+    clear(selector);
     const projects = document.querySelector(selector);
-    const label = document.createElement('label');
-    label.setAttribute('for', 'projects');
-    const select = document.createElement('select');
-    select.setAttribute('name', 'projects');
-    const option = document.createElement('option');
-    option.textContent = 'Select one';
-    select.appendChild(option);
+    const select = projBoilerPlate(selector);
     for (const [key, value] of Object.entries(arr)) {
       const option = document.createElement('option');
       option.dataset.index = key;
+      option.setAttribute('value', value);
       option.textContent = value;
       select.appendChild(option);
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './style.css';
 import dom from './dom.js';
 
-const todoItem = (title, description, dueDate, priority) => {
-  const completed = false;
-  return { title, description, dueDate, priority, completed };
+const todoItem = (
+  title,
+  description,
+  dueDate,
+  priority,
+  progress,
+  projects
+) => {
+  return { title, description, dueDate, priority, progress, projects };
 };
 
 const toDoList = [];
@@ -17,20 +23,40 @@ const addToDoItem = (item) => {
   toDoList.push(item);
 };
 
+// const newItem = createToDoItem(
+//   'clean',
+//   'clean the place',
+//   '2022'
+//   'Not Urgent'
+// );
+
+// s
+// addToDoItem(
+//   createToDoItem(
+//     'returns',
+//     'return my online shopping',
+//     new Date(2022, 6, 17),
+//     'Urgent'
+//   )
+// );
+
 const newItem = createToDoItem(
   'clean',
   'clean the place',
-  new Date(2022, 6, 14),
-  'Not Urgent'
+  '2022-08-21',
+  'low',
+  'todo',
+  ''
 );
-
 addToDoItem(newItem);
 addToDoItem(
   createToDoItem(
-    'returns',
-    'return my online shopping',
-    new Date(2022, 6, 17),
-    'Urgent'
+    'todo list',
+    'finish todo list',
+    '2022-07-17',
+    'high',
+    'doing',
+    'software developement'
   )
 );
 
@@ -39,8 +65,6 @@ const printToDo = (arr) => {
     Object.values(obj).forEach((v) => console.log(v));
   });
 };
-// toDoList.forEach(obj => Object.entries(obj).forEach(v => console.log(v)))
-// toDoList.forEach(obj => Object.entries(obj).forEach(([key, value]) => console.log(key, value)))
 
 // ? Projects
 const projects = [];
@@ -54,10 +78,23 @@ dom.render('#projects', projects);
 dom.renderFormProjects('#formProjectElem', projects);
 
 // ? Dom stuff
-document.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const form = e.target;
-  const formData = [...new FormData(form)].map(([k, v]) => v);
-  addToDoItem(formData);
-  dom.render('#todos', toDoList);
-});
+
+const eventListeners = () => {
+  const addProject = document.querySelector('#addProject');
+  addProject.addEventListener('click', () => {
+    const newProject = document.querySelector('#newProject');
+    createNewProject(newProject.value);
+    dom.render('#projects', projects);
+    dom.renderFormProjects('#formProjectElem', projects);
+  });
+  document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = [...new FormData(form)].map(([k, v]) => v);
+    console.log(formData);
+    addToDoItem(formData);
+    dom.render('#todos', toDoList);
+  });
+};
+
+eventListeners();
